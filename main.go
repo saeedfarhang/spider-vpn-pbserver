fd5af78db07cbe5e540de3c3429d88d5cebc26dd
saeedfarhang <saeedfarhang.w@gmail.com>
2024-08-20 12:44:07 +0330

    feat: add new features

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/models"
 )
 
 
@@ -22,6 +23,58 @@ func main() {
         return nil
     })
 
+
+	app.OnModelAfterCreate("orders").Add(func( e *core.ModelEvent) error {
+		app.Dao().DB().NewQuery(`UPDATE orders SET status="INCOMPLETE" WHERE id={:id}`).
+			Bind(dbx.Params{ "id": e.Model.GetId()}).Execute()
+			
+		orderId := e.Model.GetId()
+		order := e.Model.(*models.Record)
+		// Fetch the related plan's ID
+		planId := order.GetString("plan")
+		gatewayId := order.GetString("payment_gateway")
+
+		// Fetch related pricing records for the plan
+		pricings := []*models.Record{}
+		err := app.Dao().DB().NewQuery(`
+			SELECT pricing.* 
+			FROM pricing 
+			JOIN plans_pricing ON plans_pricing.pricing = pricing.id 
+			WHERE plans_pricing.plan = {:planId} AND plans_pricing.gateway = {:gatewayId}
+		`).Bind(dbx.Params{"planId": planId, "gatewayId": gatewayId}).All(&pricings)
+		if err != nil {
+			return err
+		}
+
+		// Create payments for each related pricing
+		for _, pricing := range pricings {
+			pricing, err := app.Dao().FindRecordById("pricing", pricing.Id)
+			if err != nil {
+				return err
+			}
+			
+			payments , err := app.Dao().FindCollectionByNameOrId("payments")
+			payment := models.NewRecord(payments)
+			if err != nil {
+				return err
+			}
+			
+			payment.Set("user", order.GetString("user"))
+			log.Println(pricing.Get("id"), pricing.GetFloat("price"),order.GetString("user"),pricing.GetString("currency"))
+			payment.Set("order", orderId)
+			payment.Set("amount", pricing.GetFloat("price"))
+			payment.Set("currency", pricing.GetString("currency"))
+			payment.Set("status", "UNPAID")
+
+			// Insert the payment into the database
+			if err := app.Dao().Save(payment); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+	// create outline vpn config base on selected plan
 	app.OnModelAfterCreate("vpn_configs").Add(func(e *core.ModelEvent) error {
 		app.Dao().DB().NewQuery(`UPDATE vpn_configs SET outlineConnection={:outlineLink} WHERE id={:id}`).
 			Bind(dbx.Params{ "id": e.Model.GetId(), "outlineLink": "test"}).Execute()
@@ -31,4 +84,5 @@ func main() {
     if err := app.Start(); err != nil {
         log.Fatal(err)
     }
+	
 }
