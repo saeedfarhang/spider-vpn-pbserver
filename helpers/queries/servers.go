e202acca021a457f3750f78bfc3a8e662509261e
saeedfarhang <saeedfarhang.w@gmail.com>
2024-09-04 16:13:46 +0330

    chore: cleanup formatting

--- helpers/queries/servers.go
+++ helpers/queries/servers.go
@@ -14,13 +14,13 @@ import (
 	"github.com/pocketbase/pocketbase/models"
 )
 
-func GetActiveServers (app *pocketbase.PocketBase, serverIds []string)(servers []*models.Record, err error){
-	enableStatusExpr := dbx.HashExp{"enable": true,}
+func GetActiveServers(app *pocketbase.PocketBase, serverIds []string) (servers []*models.Record, err error) {
+	enableStatusExpr := dbx.HashExp{"enable": true}
 	var idsExpr dbx.Expression
 	var query *dbx.SelectQuery
 	// Create a raw query with placeholders for each ID
-	if (len(serverIds) != 0){
-		existsCapacityExpr := dbx.Not(dbx.HashExp{"capacity":0,})
+	if len(serverIds) != 0 {
+		existsCapacityExpr := dbx.Not(dbx.HashExp{"capacity": 0})
 		placeholders := make([]string, len(serverIds))
 		params := dbx.Params{}
 		for i, id := range serverIds {
@@ -29,16 +29,16 @@ func GetActiveServers (app *pocketbase.PocketBase, serverIds []string)(servers [
 		}
 		idsExpr = dbx.NewExp(fmt.Sprintf("id IN (%s)", strings.Join(placeholders, ", ")), params)
 		query = app.Dao().RecordQuery("servers").
-		AndWhere(idsExpr).
-		AndWhere(existsCapacityExpr).
-		AndWhere(enableStatusExpr).
-		OrderBy("capacity DESC").
-		Limit(1)
-	}else {
+			AndWhere(idsExpr).
+			AndWhere(existsCapacityExpr).
+			AndWhere(enableStatusExpr).
+			OrderBy("capacity DESC").
+			Limit(1)
+	} else {
 		query = app.Dao().RecordQuery("servers").
-		AndWhere(enableStatusExpr).
-		OrderBy("capacity DESC").
-		Limit(1)
+			AndWhere(enableStatusExpr).
+			OrderBy("capacity DESC").
+			Limit(1)
 	}
 
 	if err := query.All(&servers); err != nil {
@@ -57,33 +57,32 @@ func GetActiveServers (app *pocketbase.PocketBase, serverIds []string)(servers [
 	return servers, nil
 }
 
-
-func SyncVpnConfigsRemainUsage(app *pocketbase.PocketBase)(err error){
+func SyncVpnConfigsRemainUsage(app *pocketbase.PocketBase) (err error) {
 	servers, err := GetActiveServers(app, nil)
-	if err != nil{
+	if err != nil {
 		log.Fatalf("Failed to execute query: %v", err)
 	}
 
-	for _, server := range(servers){
-		apiUrl:= server.GetString("management_api_url")
-		usages , err := outlineApi.GetAccessKeysUsages(apiUrl)
-		if err != nil{
+	for _, server := range servers {
+		apiUrl := server.GetString("management_api_url")
+		usages, err := outlineApi.GetAccessKeysUsages(apiUrl)
+		if err != nil {
 			log.Printf("Failed to get usages: %v", err)
 			continue
 		}
 		vpnConfigs := []*models.Record{}
 		err = app.Dao().RecordQuery("vpn_configs").
-						AndWhere(dbx.Not(dbx.HashExp{"connection_data": ""})).
-						// last update passed interval
-						AndWhere(dbx.NewExp("updated < {:lastUpdate}", dbx.Params{"lastUpdate": time.Now().Add(time.Hour * 1)})).
-						OrderBy("updated").
-						Limit(100).
-						All(&vpnConfigs)
-		if err != nil{
+			AndWhere(dbx.Not(dbx.HashExp{"connection_data": ""})).
+			// last update passed interval
+			AndWhere(dbx.NewExp("updated < {:lastUpdate}", dbx.Params{"lastUpdate": time.Now().Add(time.Hour * 1)})).
+			OrderBy("updated").
+			Limit(100).
+			All(&vpnConfigs)
+		if err != nil {
 			log.Fatalf("Failed to find vpn configs: %v", err)
 		}
 		log.Println("vpn configs to update remain_usage: ", vpnConfigs)
-		for _, vpnConfig := range(vpnConfigs){
+		for _, vpnConfig := range vpnConfigs {
 			connectionDataStr := vpnConfig.GetString("connection_data")
 
 			var connectionDataStruct outlineApi.AccessKey
@@ -94,14 +93,14 @@ func SyncVpnConfigsRemainUsage(app *pocketbase.PocketBase)(err error){
 			}
 			usageInByte, usageInByteExists := usages.BytesTransferredByUserID[connectionDataStruct.ID]
 			if usageInByteExists {
-				vpnConfig.Set("remain_data_mb", (vpnConfig.GetInt("usage_in_gb") * 1000) - int(usageInByte / 1e+6))
-				}else{
-				vpnConfig.Set("remain_data_mb", vpnConfig.GetInt("usage_in_gb") * 1000)
+				vpnConfig.Set("remain_data_mb", (vpnConfig.GetInt("usage_in_gb")*1000)-int(usageInByte/1e+6))
+			} else {
+				vpnConfig.Set("remain_data_mb", vpnConfig.GetInt("usage_in_gb")*1000)
 			}
 			if err := app.Dao().Save(vpnConfig); err != nil {
 				return err
 			}
 		}
-		}
+	}
 	return nil
 }
